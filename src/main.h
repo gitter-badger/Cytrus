54ba472dbfc06cec87073a93cd2155629fcbb0c9
encadyma <encadyma@gmail.com>
2015-02-15 14:38:50 -0800

    Added main.h

--- src/main.h
+++ src/main.h
@@ -1,5 +1,4 @@
-# Cytrus
-The open source game engine made by encadyma.
+/*
 
 Copyright 2015 Kevin Mo
 
@@ -14,3 +13,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. 
+
+*/
+
+#include <GL/glew.h>
